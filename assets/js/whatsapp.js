f3840359098fb46a36b8cfcf1bbb35eae576aaf8
Rudhin Menon <menonrudhin@gmail.com>
2021-06-08 15:07:45 -0400

    Sent whatsapp message using twilio API

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -4,16 +4,17 @@ function sendWhatsappMsg() {
     const key = 'fb4c46a4145103cd6e3ff4f94742fd67';
     const from_number = 'From=whatsapp:+14155238886';
     const to_number = 'To=whatsapp:+14372464771';
-    let message = 'Hi from Move';
+    let message = 'Body=Hi from Move';
 
-    let url = curl 'https://api.twilio.com/2010-04-01/Accounts/' + sid + '/Messages.json';
+    //let url = 'https://api.twilio.com/2010-04-01/Accounts/' + sid + '/Messages.json?To=whatsapp:+14372464771';
+    let url = 'https://api.twilio.com/2010-04-01/Accounts/' + sid + '/Messages.json';
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/server', true);
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true, sid, key);
 
     //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Authorization", "Basic " + btoa(sid + ':' + key))
+    //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    //xhr.setRequestHeader("Authorization", "Basic " + btoa(sid + ':' + key))
     xhr.onreadystatechange = function () { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             // Request finished. Do processing here.
@@ -21,9 +22,16 @@ function sendWhatsappMsg() {
         }
     }
 
-    const post_string = from_number + '&' + to_number + '&' + message;
+    //let post_string = from_number + '&' + to_number + '&' + message;
 
-    xhr.send(post_string);
+    let formData = new FormData();
+    //formData.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    formData.append('To','whatsapp:+14372464771');
+    formData.append('From','whatsapp:+14155238886');        
+    formData.append('Body','Hi from Move');
+
+    xhr.send(formData);
+    //xhr.send();
 }
 
-sendWhatsappMsg();
+//sendWhatsappMsg();
