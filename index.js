7501822a35ab528f3dc95d042e3c880eaa16b628
Rudhin Menon <menonrudhin@gmail.com>
2021-06-11 00:36:05 -0400

    to deploy node js instead of php, trying

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
       console.log(process.env.sid);
+      
       let body='';
       if (req.method === 'POST') {
         req.on('data', chunk => {
