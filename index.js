adb42e45415695167627e5031adad6eb895b54a3
Rudhin Menon <menonrudhin@gmail.com>
2021-06-13 22:22:02 -0400

    just server post request

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ const server = http.createServer((req, res) => {
           console.log(body);
           res.end('ok');
         });
-    } /*else {
+    } else {
       console.log('request not identified !');
-    }*/
+    }/*
     else {
-      res.render('index.html');
-    }
+	const express = require('express')
+	const path = require('path')
+	const PORT = process.env.PORT || 5000
+
+	express()
+	  .use(express.static(path.join(__dirname, 'public')))
+	  .set('views', path.join(__dirname, 'views'))
+	  .set('view engine', 'ejs')
+	  .get('/', (req, res) => res.render('pages/index'))
+	  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    }*/
 });
-server.listen($PORT);
+server.listen(PORT);
