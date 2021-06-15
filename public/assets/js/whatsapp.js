ed864cf705bfd149b0ebef82f8bd0d14140525d6
Rudhin Menon <menonrudhin@gmail.com>
2021-06-14 21:53:04 -0400

    static content using express.js

--- public/assets/js/whatsapp.js
+++ public/assets/js/whatsapp.js
@@ -2,14 +2,13 @@ function sendWhatsappMsg(message) {
     console.log('called sendWhatsappMsg')
     const from_number = 'From=whatsapp:+14155238886';
     const to_number = 'To=whatsapp:+14372464771';
-    //let message = 'Body=Hi from Move';
 
-    //let url = 'https://git.heroku.com/move-web-page.git';
-    let url = 'https://move-web-page.herokuapp.com/ver';
+    //let url = 'https://move-web-page.herokuapp.com/ver';
+    let url = 'https://move-web-page.herokuapp.com';
 
     let xhr = new XMLHttpRequest();
-    //xhr.open("POST", url, true);
-    xhr.open("GET", url, true);
+    xhr.open("POST", url, true);
+    //xhr.open("GET", url, true);
 
     //Send the proper header information along with the request
     //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -17,7 +16,7 @@ function sendWhatsappMsg(message) {
     xhr.onreadystatechange = function () { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             // Request finished. Do processing here.
-            console.log('on response');
+            console.log('SENT OK');
         }
     }
 
@@ -29,8 +28,7 @@ function sendWhatsappMsg(message) {
     formData.append('From','whatsapp:+14155238886');
     formData.append('Body', message);
 
-    //xhr.send(formData);
-    xhr.send();
+    xhr.send(formData);
     //xhr.send();
 }
 
