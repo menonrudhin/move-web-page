6d8668a13c99ae9668679733707adaf47c5f86db
Rudhin Menon <menonrudhin@gmail.com>
2021-06-14 21:04:28 -0400

    using express js to enable cors so as to accept requests from github website

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
-const http = require('http');
+//const http = require('http');
 console.log('getting started');
 const PORT=5000;
 
 var express = require('express');
 var cors = require('cors');
 var app = express();
-app.use(cors({
-  origin: 'https://menonrudhin.github.io'
-}));
 
-const server = http.createServer((req, res) => {
-      console.log('sid is  = ' , process.env.sid);
-
-      let body='';
-      if (req.method === 'POST') {
-        req.on('data', chunk => {
-          body += chunk.toString(); // convert Buffer to string
-        });
-        req.on('end', () => {
-          console.log(body);
-          res.end('ok');
-        });
-    } else {
-      console.log('request not identified !');
-    }/*
-    else {
-	const express = require('express')
-	const path = require('path')
-	const PORT = process.env.PORT || 5000
-
-	express()
-	  .use(express.static(path.join(__dirname, 'public')))
-	  .set('views', path.join(__dirname, 'views'))
-	  .set('view engine', 'ejs')
-	  .get('/', (req, res) => res.render('pages/index'))
-	  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-    }*/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://menonrudhin.github.io"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  console.log('enable cors');
 });
-server.listen(process.env.PORT || PORT);
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+  res.render('index.html');
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+ console.log('post request');
+});
+
+app.listen(process.env.PORT || PORT);
